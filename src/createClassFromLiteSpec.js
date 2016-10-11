da581849d7793eaedae7c9753298db5e78e29d9c
kwongsuphasawat <kwongsuphasawat@twitter.com>
2016-10-10 23:49:36 -0700

    fix createClassFromLiteSpec

--- src/createClassFromLiteSpec.js
+++ src/createClassFromLiteSpec.js
@@ -1,11 +1,24 @@
-import vl from 'vega-lite';
-import { createClassFromSpec } from 'react-vega';
+import React from 'react';
+import VegaLite from './VegaLite.js';
 
 // USAGE:
-// createClassFromSpec(name, spec);
-// createClassFromSpec(spec);
+// createClassFromLiteSpec(name, spec);
+// createClassFromLiteSpec(spec);
 export default function createClassFromLiteSpec(...args) {
   const spec = args.length === 1 ? args[0] : args[1];
-  const vgSpec = vl.compile(spec).spec;
-  return createClassFromSpec(vgSpec);
+
+  const propTypes = Object.assign({}, VegaLite.propTypes);
+  delete propTypes.spec;
+
+  function Chart(props) {
+    return <VegaLite spec={spec} {...props} />;
+  }
+
+  Chart.getSpec = function getSpec() {
+    return spec;
+  };
+
+  Chart.propTypes = propTypes;
+
+  return Chart;
 }
